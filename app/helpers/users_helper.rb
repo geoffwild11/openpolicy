20b513912dd130595c21c8b666155919e853c8dc
Geoff (Boris) Wild <boris@zerohour-productions.net>
2017-12-06 20:37:12 -0600

    Adding ggravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  
+  # Returns gravatar for given user 
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase) #not really necessary to downcase here since we downcase the DB. Just want to illustrate that should downcase the digest
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
